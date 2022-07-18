175c7d37e61d640aa9f446d4ed7499e262fca65a
Borna Radojčić <br52543@fer.hr>
2022-07-18 10:40:24 +0200

    Listener v2

--- listenerBorna.py
+++ listenerBorna.py
@@ -1,13 +1,13 @@
 import socket
 
 UDP_IP="10.223.204.201"
-UDP_PORT="7800"
-
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+UDP_PORT=7800
+#self.transport.getHandle().setsockopt(socket.SOL_SOCKET, 25, "ens192")
+sock = socket(socket.AF_INET,socket.SOCK_DGRAM)
 
 sock.bind((UDP_IP,UDP_PORT))
 msg=b"Hello"
 while True:
-    data,addr=sock.recvfrom(1024)
+    data, addr=sock.recvfrom(1024)
     print("Poruka: {}".format(data))
-    sock.sendto(msg,(UDP_IP,UDP_PORT))
+    sock.sendto(msg,addr)
